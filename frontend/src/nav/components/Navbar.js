ef3ab90f75f8860448a8d2dc2aa7208d398d0b69
James Mills <jimmy@buoyhealth.com>
2019-09-14 13:18:38 -0400

    Cleanup Nav

--- frontend/src/nav/components/Navbar.js
+++ frontend/src/nav/components/Navbar.js
@@ -5,75 +5,46 @@ import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 import {CrossedSwords, Magnifier, Flame, EditPerson, Flare, OpenBook} from "../../assets/svgIcons";
 
+const tabs = {
+  'Simulator': {icon: CrossedSwords, text: 'Simulator'},
+  'CombatantCreation': {icon: EditPerson, text: 'Create a combatant'},
+  'actionCreation': {icon: Flame, text: 'Action Creation', children: {
+      'AttackAgainstAC': {text: 'Attack against AC'},
+      'AttackWithSave': {text: 'Attack requiring save'}
+    }
+  },
+  'EffectCreation': {icon: Flare, text: 'Create an effect'},
+  'exploration': {icon: Magnifier, text: 'Explore', children: {
+      'exploreCombatant': 'Combatants'
+    }
+  },
+  'generalInfo': {icon: OpenBook, text: 'Information'}
+}
+
+const Icon = ({icon}) => {
+  const I = icon
+  return <I />
+}
+
 const SideNavComponent = ({setCurrentTab}) => {
   return <SideNav
     style={{backgroundColor: "#2185d0"}}
     onSelect={(selected) => {setCurrentTab(selected)}}>
     <SideNav.Toggle/>
     <SideNav.Nav defaultSelected="Simulator">
-      <NavItem eventKey="Simulator">
-        <NavIcon>
-          <CrossedSwords/>
-        </NavIcon>
-        <NavText>
-          Simulator
-        </NavText>
-      </NavItem>
-      <NavItem eventKey="CombatantCreation">
-        <NavIcon>
-          <EditPerson/>
-        </NavIcon>
-        <NavText>
-          Create a combatant
-        </NavText>
-      </NavItem>
-      <NavItem eventKey="actionCreation">
-        <NavIcon>
-          <Flame/>
-        </NavIcon>
-        <NavText>
-          Action Creation
-        </NavText>
-        <NavItem eventKey="AttackAgainstAC">
-          <NavText>
-            Attack against AC
-          </NavText>
+      {Object.keys(tabs).map(tab => (
+        <NavItem eventKey={tab}>
+          {tabs[tab].icon ? <NavIcon>
+            <Icon icon={tabs[tab].icon} />
+          </NavIcon> : <span />}
+          <NavText>{tabs[tab].text}</NavText>
+          {tabs[tab].children ? Object.keys(tabs[tab].children).map(child => (
+            <NavItem eventKey={child}>
+              <NavText>{tabs[tab].children[child].text}</NavText>
+            </NavItem>
+          )): <span />}
         </NavItem>
-        <NavItem eventKey="AttackWithSave">
-          <NavText>
-            Attack requiring save
-          </NavText>
-        </NavItem>
-      </NavItem>
-      <NavItem eventKey="EffectCreation">
-        <NavIcon>
-          <Flare/>
-        </NavIcon>
-        <NavText>
-          Create an effect
-        </NavText>
-      </NavItem>
-      <NavItem eventKey="exploration">
-        <NavIcon>
-          <Magnifier/>
-        </NavIcon>
-        <NavText>
-          Explore
-        </NavText>
-        <NavItem eventKey="exploreCombatant">
-          <NavText>
-            Combatants
-          </NavText>
-        </NavItem>
-      </NavItem>
-      <NavItem eventKey="generalInfo">
-        <NavIcon>
-          <OpenBook/>
-        </NavIcon>
-        <NavText>
-          Information
-        </NavText>
-      </NavItem>
+      ))}
     </SideNav.Nav>
   </SideNav>
 }
