2c6ae8edd7ca31535f3f47ae00e9387a371177b0
Jimmy <jimmy@pop-os.localdomain>
2020-07-27 19:20:21 -0600

    Make project work on local and server

--- frontend/src/common/sources/post.js
+++ frontend/src/common/sources/post.js
@@ -16,9 +16,11 @@ const argsToForm = (args) => {
 const http = (url, method, args, returnData=true) => {
   let data = args ? argsToForm(args) : undefined;
   // TODO: Deal with the CSRF token?
+  let prefix = window.location.hostname == 'localhost' ? 'http://localhost:8000' : 'https://api.dndfightsimulator.com'
+  console.log(prefix)
   return axios({
     method: method,
-    url: 'http://localhost:8000' + url,
+    url: prefix + url,
     data: data,
     withCredentials: false,
     headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
